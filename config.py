3ea7c2226fc1a90d586082e88a861341804d2734
lukenamop <lukenamop@gmail.com>
2022-04-04 07:18:37 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-# DISCORD credentials
+## DISCORD credentials
+# Discord bot token
 DISCORD_TOKEN = 'TOKEN'
 
-# BOT variables
+## BOT variables
+# Discord server ID where the bot will be active
+SERVER_ID = 
+# Discord channel ID where log messages will be send
 LOG_CHANNEL_ID = 
+# True/False
 ENABLE_WEBHOOK_PROTECTION = True
 ALLOW_VERIFIED_BOT_WEBHOOKS = False
